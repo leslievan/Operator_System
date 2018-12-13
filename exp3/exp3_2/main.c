ff5e5062335e02412dda84f9853b9d311549ac9d
Leslie Van <leslie.van@outlook.com>
2018-12-13 20:36:39 +0800

    Change a little

--- exp3/exp3_2/main.c
+++ exp3/exp3_2/main.c
@@ -8,6 +8,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
 #include <errno.h>
 #include <semaphore.h>
 #include <string.h>
@@ -36,8 +38,9 @@ int main(int argc, char **argv) {
     int flag = 0;
     ssize_t n;
     char buf[BUF_MAX_SIZE];
-    sem_t write_mutex;
-    sem_init(&write_mutex, 1, 1);
+    char str[BUF_MAX_SIZE];
+    sem_t *write_mutex;
+    write_mutex = sem_open("pipe_test_wm", O_CREAT | O_RDWR, 0666, 1);
 
     for (i = 0; i < BUF_MAX_SIZE; i++) {
         buf[i] = 'a';
@@ -48,7 +51,7 @@ int main(int argc, char **argv) {
 
     if (pid == 0) {
         int count = 0;
-        sem_wait(&write_mutex);
+        sem_wait(write_mutex);
         close(pipefd[0]);
         int flags = fcntl(pipefd[1], F_GETFL);
         fcntl(pipefd[1], F_SETFL, flags | O_NONBLOCK);
@@ -62,46 +65,54 @@ int main(int argc, char **argv) {
             }
         }
         printf("space = %dKB\n", (count * BUF_MAX_SIZE) / 1024);
-        sem_post(&write_mutex);
+        sem_post(write_mutex);
         exit(0);
     } else {
         CHECK((pid = fork()) >= 0);
 
         if (pid == 0) {
             sleep(1);
-            sem_wait(&write_mutex);
+            sem_wait(write_mutex);
             close(pipefd[0]);
             n = write(pipefd[1], "This is the second children.\n", 29);
             printf("children 2 write %dB\n", n);
-            sem_post(&write_mutex);
+            sem_post(write_mutex);
             exit(0);
         } else {
             CHECK((pid = fork()) >= 0);
 
             if (pid == 0) {
                 sleep(1);
-                sem_wait(&write_mutex);
+                sem_wait(write_mutex);
                 close(pipefd[0]);
                 n = write(pipefd[1], "This is the third children.\n", 28);
                 printf("children 3 write %dB\n", n);
-                sem_post(&write_mutex);
+                sem_post(write_mutex);
                 exit(0);
             } else {
                 wait(0);
                 close(pipefd[1]);
-                for (i = 0; i < 57; i++) {
-                    while (!flag) {
-                        n = read(pipefd[0], buf, BUF_MAX_SIZE);
-                        if (n == 0) {
-                            flag = 1;
-                        } else {
-                            printf("%dB read\n", n);
-                        }
+                int flags = fcntl(pipefd[0], F_GETFL);
+                fcntl(pipefd[0], F_SETFL, flags | O_NONBLOCK);
+                while (!flag) {
+                    n = read(pipefd[0], str, BUF_MAX_SIZE);
+                    if (n == -1) {
+                        flag = 1;
+                    } else {
+                        printf("%dB read\n", n);
                     }
-                    printf("%c", buf[i]);
+                }
+                sleep(2);
+                n = read(pipefd[0], str, BUF_MAX_SIZE);
+                printf("%dB read\n", n);
+                for (i = 0; i < n; i++) {
+                    printf("%c", str[i]);
                 }
             }
         }
     }
+
+    sem_close(write_mutex);
+    sem_unlink("pipe_test_wm");
     return 0;
 }
