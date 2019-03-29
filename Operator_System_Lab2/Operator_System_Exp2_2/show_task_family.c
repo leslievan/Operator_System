4c95050b4d75245950d66748c3c82d847e0eb621
Arcana <leslie.van@outlook.com>
2019-03-29 20:27:31 +0800

    change parent->real_parent

--- Operator_System_Lab2/Operator_System_Exp2_2/show_task_family.c
+++ Operator_System_Lab2/Operator_System_Exp2_2/show_task_family.c
@@ -18,6 +18,7 @@ static int __init show_task_family_init(void)
     struct pid *ppid;
     struct task_struct *p;
     struct task_struct *pos;
+    char *ptype[4] = {"[I]", "[P]", "[S]", "[C]"};
 
     // 通过进程的PID号pid一步步找到进程的进程控制块p
     ppid = find_get_pid(pid);
@@ -32,19 +33,23 @@ static int __init show_task_family_init(void)
     printk("%-10s%-20s%-6s%-6s\n", "Type", "Name", "PID", "State");
     printk("------------------------------------------\n");
 
+    // Itself
+    // 打印自身信息
+    printk("%-10s%-20s%-6d%-6d\n", ptype[0], p->comm, p->pid, p->state);
+
     // Parent
     // 打印父进程信息
-    printk("%-10s%-20s%-6d%-6d\n", "[P]", p->parent->comm, p->parent->pid,
-           p->parent->state);
+    printk("%-10s%-20s%-6d%-6d\n", ptype[1], p->real_parent->comm,
+           p->real_parent->pid, p->real_parent->state);
 
     // Siblings
     // 遍历父进程的子，即我的兄弟进程，输出信息
     // “我”同样是父进程的子进程，所以当二者进程PID号一致时，跳过不输出
-    list_for_each_entry(pos, &(p->parent->children), sibling)
+    list_for_each_entry(pos, &(p->real_parent->children), sibling)
     {
         if (pos->pid == pid)
             continue;
-        printk("%-10s%-20s%-6d%-6d\n", "[S]", pos->comm, pos->pid,
+        printk("%-10s%-20s%-6d%-6d\n", ptype[2], pos->comm, pos->pid,
                pos->state);
     }
 
@@ -52,9 +57,11 @@ static int __init show_task_family_init(void)
     // 遍历”我“的子进程，输出信息
     list_for_each_entry(pos, &(p->children), sibling)
     {
-        printk("%-10s%-20s%-6d%-6d\n", "[C]", pos->comm, pos->pid,
+        printk("%-10s%-20s%-6d%-6d\n", ptype[3], pos->comm, pos->pid,
                pos->state);
     }
+
+    return 0;
 }
 
 static void __exit show_task_family_exit(void)
